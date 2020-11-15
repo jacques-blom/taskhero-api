367a09239dcd2e4080ed4dd960f8d69589f0424c
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-11-15 14:23:17 +0200

    Fix get-tasks and add insert-task

--- handlers/insert-task.ts
+++ handlers/insert-task.ts
@@ -1,41 +1,53 @@
 import {DynamoDB} from 'aws-sdk'
+import {v4} from 'uuid'
 
 const dynamoDB = new DynamoDB.DocumentClient()
 
-const getTasks = async (userId: string) => {
-    const state = await dynamoDB
-        .query({
+type TaskInput = {
+    userId: string
+    label: string
+}
+
+const insertTask = async ({userId, label}: TaskInput) => {
+    if (!userId) throw new Error('Missing userId')
+    if (!label) throw new Error('Missing label')
+
+    const newTask = {
+        id: v4(),
+        userId,
+        label,
+        completed: false,
+    }
+
+    await dynamoDB
+        .put({
             TableName: process.env.DYNAMODB_TABLE_TASKS!,
-            IndexName: 'UserIdIndex',
-            KeyConditionExpression: 'userId = :userId',
-            ExpressionAttributeValues: {
-                ':userId': userId,
-            },
+            Item: newTask,
         })
         .promise()
 
-    return state.Items || []
+    return newTask
 }
 
 export const handler = async (event: any) => {
     try {
-        const userId = event.queryStringParameters?.userId
-        if (!userId) throw new Error('Not found')
+        if (!event.body) throw new Error('Missing body')
 
-        const body = await getTasks(userId)
+        const task = JSON.parse(event.body)
+        const body = await insertTask(task)
 
         return {
             statusCode: 200,
-            body: JSON.stringify(body),
+            body,
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
             },
         }
     } catch (error) {
-        if (error.message === 'Not found') {
+        if (error.message) {
             return {
-                statusCode: 404,
+                statusCode: 500,
                 body: error.message,
             }
         }
